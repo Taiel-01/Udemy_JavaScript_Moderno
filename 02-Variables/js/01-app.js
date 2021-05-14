55a9b50a684be20709ccefb8f7e06ab863531a1b
Taiel Nunes <taielalen56@hotmail.com>
2021-05-13 21:36:59 -0300

    02 - Completo

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,34 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 Pulgadas';
+console.log(producto)
+
+// JavaScript es un lenguaje de tipo dinamico, no se especifica el tipo de dato
+producto = 20;
+console.log(producto);
+
+//Se pueden inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+
+disponible = false;
+
+// Inicializar multiples variables
+// var categoria = 'Computadoras',
+//     marca = 'Marca famosa',
+//     calificacion = 5;
+
+// var dias99;
+
+// var 01__;
+// var __01;
+// var _01;
+// var _usuario;
+// var __usuario;
+
+// var nombreProductoCategoriaPrecio;
+// var nombre_producto_categoria_precio;
+// var Producto;
