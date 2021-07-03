dd30d90e63be19be1feb8a4389a4c2c50de808b5
Taiel Nunes <taielalen56@hotmail.com>
2021-07-02 22:46:47 -0300

    42 - Completo

--- 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
+++ 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
@@ -48,19 +48,28 @@ function mostrarAlerta(mensaje) {
 
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
-    const key = '';
+    const key = '22250953-c4cbfd9bce53be9f7234b629e';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })
+    //* fetch(url)
+    //*     .then(respuesta => respuesta.json())
+    //*     .then(resultado => {
+    //*        totalPaginas = calcularPaginas(resultado.totalHits);
+    //*         mostrarImagenes(resultado.hits);
+    //*     });
+
+    try {
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
