e97c581410c44622d804d89ce551fa9a77825fab
Taiel Nunes <taielalen56@hotmail.com>
2021-07-07 19:35:11 -0300

    48 - Completo

---
+++ 48-DesignPatterns/js/01-app.js
@@ -0,0 +1,13 @@
+// class Pattern
+
+class Persona {
+    constructor(nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+const persona = new Persona('Taiel', 'correo@correo.com');
+
+console.log(persona);
+
